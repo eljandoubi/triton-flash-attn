1f7aeddb4e458d1619da7003826c6b620bd5d261
eljandoubi <abdelkarim.eljandoubi@ensta-paris.fr>
2025-03-08 13:33:07 +0100

    check triton cuda

--- tests/test_env.py
+++ tests/test_env.py
@@ -12,7 +12,13 @@ def test_module(name: str):
     assert ut.find_spec(name) is not None
 
 
-def test_cuda():
-    """check cuda availability"""
+def test_torch_cuda():
+    """check cuda availability under torch"""
     from torch import cuda   # pylint: disable=C0415
     assert cuda.is_available()
+
+
+def test_triton_cuda():
+    """check cuda availability under triton"""
+    import triton   # pylint: disable=C0415
+    assert triton.runtime.driver.active.get_current_target().backend == "cuda"
